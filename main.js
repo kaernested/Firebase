38125ebe519352b521d31d6c297e2b8d2e155a62
Óskin <osk.bjorns90@gmail.com>
2019-03-27 10:12:56 +0000

    from Osk

---
+++ main.js
@@ -0,0 +1,27 @@
+// Add a new document in collection "cities"
+const db = firebase.firestore();
+
+db.collection("cities").doc("LA").set({
+    name: "Los Angeles",
+    state: "CA",
+    country: "USA"
+})
+.then(()=> {
+    console.log("Document successfully written!");
+})
+.catch((error)=> {
+    console.error("Error writing document: ", error);
+});
+
+const button = document.querySelector('button');
+button.onclick = ()=> {
+    let nameValue = document.getElementById('name').value;
+    let urlValue = document.getElementById('url').value;
+    console.log(nameValue);
+    console.log(urlValue);
+
+    db.collection("images").doc(nameValue).set({
+        name: nameValue,
+        url: urlValue,
+    })
+} 
